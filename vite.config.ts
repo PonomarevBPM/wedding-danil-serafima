64240c2c08552186eb208d070a421dad38960050
PonomarevBPM <ponomarev.bpm@gmail.com>
2025-04-13 13:37:28 -0700

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default ({ mode }: any) =>
     defineConfig({
         server: {
@@ -17,5 +17,5 @@ export default ({ mode }: any) =>
                         : 'http://localhos:5174/'
             }
         },
-        base: mode === 'production' ? '/SA/dist/' : '/'
+        base: mode === 'production' ? '/DS/dist/' : '/'
     });
